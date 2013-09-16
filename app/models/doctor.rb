2e1b7ebc7daf318ed6562827ad8980336325db17
gregmalcolm <greg@gregmalcolm.com>
2013-09-15 22:01:23 -0400

    Created page to demonstrate Rails 4 form helper changes

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,12 @@
 class Doctor < ActiveRecord::Base
   has_many :companions
+
+  #Fakery
+  attr_accessor :pocket_contents
+  attr_accessor :doctor_number
+  attr_accessor :date
+  attr_accessor :week
+  attr_accessor :month
+  attr_accessor :colour
+  attr_accessor :date_field
 end
